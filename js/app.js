66fcc048a5739d9b5fe5926b59cd981239a0572c
danielmtz <martinezdaniel554@gmail.com>
2022-05-21 14:18:12 -0500

    cargando archvos

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/*let ingresaNumero = Number(prompt('Ingresa un numero'))
+
+
+for (let i = 0; i < 11; i++){
+    let resultado = ingresaNumero + i
+    console.log(ingresaNumero + ' + ' + i + ' = ' + resultado)
+}
+
+
+let entrada = prompt('Ingrese un dato')
+while(entrada != "ESC"){
+    console.log('El usuario ingreso ' + entrada);
+    entrada = prompt('Ingrese otro dato')
+}
+
+let i = 0
+let numero = Number(prompt('Ingresa un numero'))
+
+do{
+    i+=1
+    console.log('Hola')
+}while( i >= numero)
+*/
+/* OBJETOS */
