5b8baa85561945fb713b96b34458ce5a21436289
danielmtz <martinezdaniel554@gmail.com>
2022-05-24 11:51:37 -0500

    agregando primer desafio

--- js/app.js
+++ js/app.js
@@ -22,3 +22,59 @@ do{
 }while( i >= numero)
 */
 /* OBJETOS */
+/*const personal = {
+    nombre: prompt('Ingresa tu nombre'),
+    edad: Number(prompt('Ingresa tu edad')),
+    calle: prompt('Ingresa tu direccion')
+}
+
+console.log(personal)
+alert('Bienvenido ' + nombre + '!')*/
+
+/*******************Defafio 1********************/
+
+let nombre = prompt('Ingresa tu nombre')
+let apellido = prompt('Ingresa tu apellido')
+let domicilio = prompt('Ingresa tu domicilio')
+
+function saludo( nombreUsuario, apellidoUsuario, domicilioUsuario){
+    alert('Bienvenido a videogame planet  ' + nombreUsuario + ' ' + apellidoUsuario)
+    console.log('apellido: ' + apellidoUsuario + ', ' + 'nombre: ' + nombreUsuario + 'domicilio: ' + domicilioUsuario)
+}
+
+saludo(nombre, apellido, domicilio)
+
+alert('para continuar da click en aceptar e ingresa nombre del juego, plataforma y precio (solo podras seleccionar 3 juegos a la vez)')
+
+let nombreJuego = prompt('Ingresa el nombre del videojuego que deseas comprar')
+let plataforma = prompt('Ingresa la plataforma en la que deseas comprar el videojuego')
+let precio = Number(prompt('Ingresa el precio del juego'))
+
+function ordenJuego1(nombreVideojuego, plataformaVideojuego, precioVideojuego){
+    alert('El juego que deseas adquirir es ' + nombreVideojuego + ' para ' + plataformaVideojuego + ' con un costo de ' + precioVideojuego)
+}
+
+ordenJuego1(nombreJuego,plataforma,precio)
+
+let nombreJuego2 = prompt('Ingresa el nombre del videojuego que deseas comprar')
+let plataforma2 = prompt('Ingresa la plataforma en la que deseas comprar el videojuego')
+let precio2 = Number(prompt('Ingresa el precio del juego'))
+function ordenJuego2(nombreVideojuego, plataformaVideojuego, precioVideojuego){
+    alert('El juego que deseas adquirir es ' + nombreVideojuego + ' para ' + plataformaVideojuego + ' con un costo de ' + precioVideojuego)
+}
+    ordenJuego2(nombreJuego2,plataforma2,precio2)
+
+let nombreJuego3 = prompt('Ingresa el nombre del videojuego que deseas comprar')
+let plataforma3 = prompt('Ingresa la plataforma en la que deseas comprar el videojuego')
+let precio3 = Number(prompt('Ingresa el precio del juego'))
+function ordenJuego3(nombreVideojuego, plataformaVideojuego, precioVideojuego){
+    alert('El juego que deseas adquirir es ' + nombreVideojuego + ' para ' + plataformaVideojuego + ' con un costo de ' + precioVideojuego)
+}
+ordenJuego3(nombreJuego3,plataforma3,precio3)
+
+function totalPago(precioJuego1, precioJuego2, precioJuego3){
+    return(precioJuego1 + precioJuego2 + precioJuego3)
+}
+totalPago(precio,precio2,precio3)
+
+alert('tu total a pagar es de ' + totalPago(precio,precio2,precio3))
